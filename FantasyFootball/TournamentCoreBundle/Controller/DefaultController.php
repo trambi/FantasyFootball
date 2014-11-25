54d0d76216716b1060846731ab85721ea1eafca9
trambi <bertrand.madet@gmail.com>
2014-11-26 00:38:40 +0100

    Correction of the first issue

--- FantasyFootball/TournamentCoreBundle/Controller/DefaultController.php
+++ FantasyFootball/TournamentCoreBundle/Controller/DefaultController.php
@@ -3,11 +3,33 @@
 namespace FantasyFootball\TournamentCoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('FantasyFootballTournamentCoreBundle:Default:index.html.twig', array('name' => $name));
-    }
+	public function indexAction()
+   {
+		/** @var $router \Symfony\Component\Routing\Router */
+		$router = $this->container->get('router');
+	   /** @var $collection \Symfony\Component\Routing\RouteCollection */
+    	$collection = $router->getRouteCollection();
+    	$allRoutes = $collection->all();
+
+    	$routes = array();
+
+
+		/** @var $params \Symfony\Component\Routing\Route */
+    	foreach ($allRoutes as $route => $params)
+    	{
+			$defaults = $params->getDefaults();
+
+			if (isset($defaults['_controller']))
+			{
+        		if ( 0 === strncmp($defaults['_controller'],"FantasyFootball\\TournamentCoreBundle",strlen("FantasyFootball\\TournamentCoreBundle") ) ){
+        			$routes[$route]=$params->getPath();	
+        		}
+			}
+		}
+		return new JsonResponse($routes);
+	}
 }
