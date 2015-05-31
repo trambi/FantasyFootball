9147df5988c59ead0305b2b02b92a72a3ea28e29
Trambi <Trambi@trambi-laptop>
2015-06-01 00:44:54 +0200

    Ajout de requetes

--- TournamentAdminBundle/Util/DataUpdater.php
+++ TournamentAdminBundle/Util/DataUpdater.php
@@ -12,7 +12,15 @@ class DataUpdater extends DataProvider
 	const insertCoachQuery ='INSERT INTO tournament_coach (team_name,name,id_race,email,naf_number,edition,ready,id_coach_team) VALUES(?,?,?,?,?,?,?,?)';
 	const updateCoachQuery ='UPDATE tournament_coach SET team_name=?,name=?,id_race=?,email=?,naf_number=?,edition=?,ready=? WHERE id=?';
 	const deleteCoachQuery ='DELETE tournament_coach WHERE id=?';
-	
+        const setReadyCoachQuery='UPDATE tournament_coach SET ready = 1';
+        const insertMatchQuery ='INSERT INTO tournament_match (id_coach_1,id_coach_2,round,edition,table_number) VALUES(?,?,?,?,?)';
+	const resumeMatchQuery ='UPDATE tournament_match SET td_1=?,td_2=?,sortie_1=?,sortie_2=?,points_1=?,points_2=?,status=\'resume\'	WHERE id_match=?';
+	const deleteMatchQuery ='DELETE FROM tournament_match';
+	const updateRankingQuery ='UPDATE INTO tournament_coach SET points=?,opponents_points=?,net_td=?,casualties=? WHERE id=?';
+	const deletepreCoachQuery ='DELETE FROM tournament_precoach';
+	const updateTeamQuery ='UPDATE INTO tournament_coach SET team_name=?,name=?,id_race=?,email=?,fan_factor=?,naf_number=?,edition=?,ready=? WHERE id=?';
+
+        
 	public function insertCoach($team){
 		$teams = array($team);
 		$this->insertCoachs($teams);
