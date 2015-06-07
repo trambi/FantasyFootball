e6605e02b2212ac0131a4e247cce16c277a76fea
trambi <bertrand.madet@gmail.com>
2015-06-07 19:12:35 +0200

    Modification de CoachTeam

--- TournamentAdminBundle/Controller/CoachTeamController.php
+++ TournamentAdminBundle/Controller/CoachTeamController.php
@@ -4,6 +4,9 @@ namespace FantasyFootball\TournamentAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use FantasyFootball\TournamentCoreBundle\Util\DataProvider;
+use FantasyFootball\TournamentAdminBundle\Util\DataUpdater;
+
 class CoachTeamController extends Controller
 {
     public function AddAction()
@@ -29,5 +32,23 @@ class CoachTeamController extends Controller
         return $this->render('FantasyFootballTournamentAdminBundle:CoachTeam:Load.html.twig', array(
                 // ...
             ));    }
-
+    public function viewAction($coachTeamId)
+    {
+	$conf = $this->get('fantasy_football_core_db_conf');
+    	$data = new DataUpdater($conf);
+    	$coachTeam = $data->getCoachTeamById($coachTeamId);
+    	$matchs = $data->getMatchsByCoachTeam($coachTeamId);
+	return $this->render('FantasyFootballTournamentAdminBundle:CoachTeam:View.html.twig', array(
+  		'coachTeam'=>$coachTeam,'matchs'=>$matchs));
+    }
+      
+    public function ListAction($edition)
+    {
+        $conf = $this->get('fantasy_football_core_db_conf');
+	$data = new DataProvider($conf);
+        $coachTeams = $data->getCoachTeamsByEdition($edition);
+	return $this->render('FantasyFootballTournamentAdminBundle:CoachTeam:List.html.twig', array(
+      	'coachTeams' => $coachTeams, 'edition'=>$edition));
+    }
 }
+?>
