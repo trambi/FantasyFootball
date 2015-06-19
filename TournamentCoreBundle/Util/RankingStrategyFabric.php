90f7b9e0ebcd5392b9b755117c533647346a8da3
trambi <bertrand.madet@gmail.com>
2015-06-19 02:42:13 +0200

    Ajout de la stratégie du RDVBB13

--- TournamentCoreBundle/Util/RankingStrategyFabric.php
+++ TournamentCoreBundle/Util/RankingStrategyFabric.php
@@ -8,23 +8,27 @@ use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategySixthToEighth
 use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategyNinthToTenth;
 use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategyEleventh;
 use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategyTwelfth;
+use FantasyFootball\TournamentCoreBundle\Util\Rdvbb\RankingStrategyThirhteenth;
 
 class RankingStrategyFabric {
     static function getByName($name) {
         $strategy = null;
-        if ('RdvbbFirst' === $name) {
+        if ( 'RdvbbFirst' === $name ) {
             $strategy = new RankingStrategyFirst();
-        } else if ('RdvbbSecondToFifth' === $name) {
+        } else if ( 'RdvbbSecondToFifth' === $name ) {
             $strategy = new RankingStrategySecondToFifth();
-        } else if ('RdvbbSixthToEighth' === $name) {
+        } else if ( 'RdvbbSixthToEighth' === $name ) {
             $strategy = new RankingStrategySixthToEighth();
-        } else if ('RdvbbNinthToTenth' === $name) {
+        } else if ( 'RdvbbNinthToTenth' === $name ) {
             $strategy = new RankingStrategyNinthToTenth();
-        } else if ('RdvbbEleventh' === $name) {
+        } else if ( 'RdvbbEleventh' === $name ) {
             $strategy = new RankingStrategyEleventh();
-        } else if ('RdvbbTwelfth' === $name) {
+        } else if ( 'RdvbbTwelfth' === $name ) {
             $strategy = new RankingStrategyTwelfth();
+        } else if ( 'RdvbbThirhteenth' === $name ){
+            $strategy = new RankingStrategyThirhteenth();
         }
+        
         return $strategy;
     }
 
