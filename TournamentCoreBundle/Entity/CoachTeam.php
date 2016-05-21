98304b12fead4e222122d2d7722a60c3ec1a6d5b
trambi <bertrand.madet@gmail.com>
2016-05-21 23:47:01 +0200

    Indentation correction

--- TournamentCoreBundle/Entity/CoachTeam.php
+++ TournamentCoreBundle/Entity/CoachTeam.php
@@ -12,96 +12,95 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class CoachTeam
 {
-    /**
-     * @ORM\OneToMany(targetEntity="Coach", mappedBy="coachTeam")
-     */
-    protected $coachs;
+  /**
+   * @ORM\OneToMany(targetEntity="Coach", mappedBy="coachTeam")
+   */
+  protected $coachs;
 
-    public function __construct()
-    {
-        $this->coachs = new ArrayCollection();
-    }
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  public function __construct()
+  {
+      $this->coachs = new ArrayCollection();
+  }
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255)
-     */
-    private $name;
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="name", type="string", length=255)
+   */
+  private $name;
 
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * Get id
+   *
+   * @return integer 
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
 
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return CoachTeam
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
+  /**
+   * Set name
+   *
+   * @param string $name
+   * @return CoachTeam
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+    return $this;
+  }
 
-        return $this;
-    }
+  /**
+   * Get name
+   *
+   * @return string 
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
 
-    /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+  /**
+   * Add coach
+   *
+   * @param \FantasyFootball\TournamentCoreBundle\Entity\Coach $coach
+   * @return CoachTeam
+   */
+  public function addCoach(\FantasyFootball\TournamentCoreBundle\Entity\Coach $coach)
+  {
+    $this->coachs[] = $coach;
+    $coach->setCoachTeam($this);
+    return $this;
+  }
 
-    /**
-     * Add coach
-     *
-     * @param \FantasyFootball\TournamentCoreBundle\Entity\Coach $coach
-     * @return CoachTeam
-     */
-    public function addCoach(\FantasyFootball\TournamentCoreBundle\Entity\Coach $coach)
-    {
-        $this->coachs[] = $coach;
-        $coach->setCoachTeam($this);
-        return $this;
-    }
+  /**
+   * Remove coachs
+   *
+   * @param \FantasyFootball\TournamentCoreBundle\Entity\Coach $coachs
+   */
+  public function removeCoach(\FantasyFootball\TournamentCoreBundle\Entity\Coach $coach)
+  {
+    $this->coachs->removeElement($coach);
+    $coach->setCoachTeam();
+  }
 
-    /**
-     * Remove coachs
-     *
-     * @param \FantasyFootball\TournamentCoreBundle\Entity\Coach $coachs
-     */
-    public function removeCoach(\FantasyFootball\TournamentCoreBundle\Entity\Coach $coach)
-    {
-        $this->coachs->removeElement($coach);
-        $coach->setCoachTeam();
-    }
-
-    /**
-     * Get coachs
-     *
-     * @return \Doctrine\Common\Collections\Collection 
-     */
-    public function getCoachs()
-    {
-        return $this->coachs;
-    }
+  /**
+   * Get coachs
+   *
+   * @return \Doctrine\Common\Collections\Collection 
+   */
+  public function getCoachs()
+  {
+    return $this->coachs;
+  }
 }
