826e4731ba44a381fde8bd04a83fc1b1df6f56d7
trambi <bertrand.madet@gmail.com>
2016-05-23 22:57:16 +0200

    Reindent CoachPairingContext

--- TournamentAdminBundle/Util/CoachPairingContext.php
+++ TournamentAdminBundle/Util/CoachPairingContext.php
@@ -19,35 +19,34 @@ use Doctrine\ORM\EntityManager;
  * @author Trambi
  */
 abstract class CoachPairingContext extends PairingContext implements IPairingContext {
-    
-    public function __construct(Edition $edition,
-            EntityManager $em,
-            DatabaseConfiguration $conf)
-    {
-        parent::__construct($edition, $em, $conf);
+
+  public function __construct(Edition $edition,EntityManager $em,DatabaseConfiguration $conf)
+  {
+    parent::__construct($edition, $em, $conf);
+  }
+
+  public function init()
+  {
+    return $this->customInit();
+  }
+
+  public function persist(Array $games,$round){
+    $table = 1;
+    $edition = $this->edition->getId();
+    $coachsById = $this->$coachsById;
+    foreach($games as $coachGame){
+      $rosterId = $coachGame[0];
+      $opponentRosterId = $coachGame[1];
+      $game = new Game();
+      $game->setEdition($edition);
+      $game->setRound($round);
+      $game->setTableNumber($table);
+      $game->setCoach1($coachsById[$rosterId]);
+      $game->setCoach2($coachsById[$opponentRosterId]);
+      $em->persist($game);
+      $table ++;
     }
-    
-    public function init()
-    {
-        return $this->customInit();
-    }
-    
-    public function persist(Array $games,$round){
-        $table = 1;
-        $edition = $this->edition->getId();
-        $coachsById = $this->$coachsById;
-        foreach($games as $coachGame){
-                $rosterId = $coachGame[0];
-                $opponentRosterId = $coachGame[1];
-                $game = new Game();
-                $game->setEdition($edition);
-                $game->setRound($round);
-                $game->setTableNumber($table);
-                $game->setCoach1($coachsById[$rosterId]);
-                $game->setCoach2($coachsById[$opponentRosterId]);
-                $em->persist($game);
-                $table ++;
-            }
-    }
-    abstract protected function customInit();
+  }
+
+  abstract protected function customInit();
 }
