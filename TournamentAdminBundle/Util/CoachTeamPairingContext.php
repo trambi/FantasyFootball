866ebf038310d00887a021db5c94f422e0ec336d
trambi <bertrand.madet@gmail.com>
2016-05-23 22:58:18 +0200

    Change table number incrementation
    CoachTeam +10
    Coach +1

--- TournamentAdminBundle/Util/CoachTeamPairingContext.php
+++ TournamentAdminBundle/Util/CoachTeamPairingContext.php
@@ -19,56 +19,52 @@ use Doctrine\ORM\EntityManager;
  * @author Trambi
  */
 abstract class CoachTeamPairingContext extends PairingContext implements IPairingContext {
-    
-    protected $coachsById;
-    protected $sortedCoachsByTeamCoachId;
-    
-    public function __construct(Edition $edition,
-            EntityManager $em,
-            DatabaseConfiguration $conf)
-    {
-        parent::__construct($edition, $em, $conf);
-        $this->coachsById = array();
-        $this->sortedCoachsByTeamCoachId = array();
-    }
-    
-    public function init()
-    {
-        $editionId = $this->edition->getId();
-        $coachs = $this->em->getRepository('FantasyFootballTournamentCoreBundle:Coach')
-                ->findByEdition($editionId);
-        foreach ($coachs as $coach)
-        {
-            $this->coachsById[$coach->getId()] = $coach;
-        }
-        return $this->customInit();
-    }
-    
-    abstract protected function customInit();
-    
-    public function persist(Array $games,$round) {
-        $coachsById = $this->coachsById;
-        $sortedCoachsByTeamCoachId = $this->sortedCoachsByTeamCoachId;
-        $editionId = $this->edition->getId();
-        $table = 1;
-        foreach ($games as $teamGame) {
-            $teamCoachId1 = $teamGame[0];
-            $teamCoachId2 = $teamGame[1];
 
-            foreach ($sortedCoachsByTeamCoachId[$teamCoachId1] as $index => $rosterId) {
-                $opponentRosterIds = $sortedCoachsByTeamCoachId[$teamCoachId2];
-                $opponentRosterId = $opponentRosterIds[$index];
-                $game = new Game();
-                $game->setEdition($editionId);
-                $game->setRound($round);
-                $game->setTableNumber($table);
-                $game->setCoach1($coachsById[$rosterId]);
-                $game->setCoach2($coachsById[$opponentRosterId]);
-                $this->em->persist($game);
+  protected $coachsById;
+  protected $sortedCoachsByTeamCoachId;
 
-                $table ++;
-            }
-        }
+  public function __construct(Edition $edition,EntityManager $em,DatabaseConfiguration $conf){
+    parent::__construct($edition, $em, $conf);
+    $this->coachsById = array();
+    $this->sortedCoachsByTeamCoachId = array();
+  }
+
+  public function init(){
+    $editionId = $this->edition->getId();
+    $coachs = $this->em->getRepository('FantasyFootballTournamentCoreBundle:Coach')
+                        ->findByEdition($editionId);
+    foreach ($coachs as $coach){
+      $this->coachsById[$coach->getId()] = $coach;
     }
+    return $this->customInit();
+  }
+
+  abstract protected function customInit();
+
+  public function persist(Array $games,$round) {
+    $coachsById = $this->coachsById;
+    $sortedCoachsByTeamCoachId = $this->sortedCoachsByTeamCoachId;
+    $editionId = $this->edition->getId();
+    $teamGameIndex = 1;
+    foreach ( $games as $teamGame ) {
+      $teamCoachId1 = $teamGame[0];
+      $teamCoachId2 = $teamGame[1];
+      $gameIndex = 1;
+      foreach ( $sortedCoachsByTeamCoachId[$teamCoachId1] as $index => $rosterId ) {
+        $opponentRosterIds = $sortedCoachsByTeamCoachId[$teamCoachId2];
+        $opponentRosterId = $opponentRosterIds[$index];
+        $game = new Game();
+        $game->setEdition($editionId);
+        $game->setRound($round);
+        $game->setTableNumber( ($teamGameIndex * 10) + $gameIndex );
+        $game->setCoach1($coachsById[$rosterId]);
+        $game->setCoach2($coachsById[$opponentRosterId]);
+        $this->em->persist($game);
+
+        $gameIndex ++;
+      }
+      $teamGameIndex ++;
+    }
+  }
 
 }
