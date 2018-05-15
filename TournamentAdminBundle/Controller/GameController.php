989d6852e65348efa2157452adaf593974f4e7f5
Trambi <bertrand.madet@gmail.com>
2018-05-16 00:22:22 +0200

    Fix #35 by adding action to summarize matchs

--- TournamentAdminBundle/Controller/GameController.php
+++ TournamentAdminBundle/Controller/GameController.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    FantasyFootball Symfony2 bundles - Symfony2 bundles collection to handle fantasy football tournament 
+    FantasyFootball Symfony2 bundles - Symfony2 bundles collection to handle fantasy football tournament
     Copyright (C) 2017  Bertrand Madet
 
     This program is free software: you can redistribute it and/or modify
@@ -153,7 +153,7 @@ class GameController extends Controller
     return $this->render('FantasyFootballTournamentAdminBundle:Game:resume.html.twig', array(
                         'form' => $form->createView(),
                         'game' => $game,
-                        'edition'=> $game->getEdition()) );    
+                        'edition'=> $game->getEdition()) );
   }
 
   protected function createCompleteForm($game){
@@ -210,12 +210,12 @@ class GameController extends Controller
               ->getForm();
     return $form;
   }
-  
+
   public function modifyAction(Request $request,$gameId)
   {
     $em = $this->getDoctrine()->getManager();
     $game = $em->getRepository('FantasyFootballTournamentCoreBundle:Game')->findOneById($gameId);
-    
+
     $form = $this->createCompleteForm($game);
     $form->handleRequest($request);
     if ( $form->isValid() ) {
@@ -238,10 +238,69 @@ class GameController extends Controller
                         'edition'=> $game->getEdition()) );
   }
 
-  public function viewAction()
+  protected function _gamesByEditionAndRound($edition, $round)
   {
-    return array(
-      // ...
-    );
+      $em = $this->getDoctrine()->getManager();
+      $criterias = ["edition"=>$edition,"round"=>$round];
+      $order = ["tableNumber"=>"asc","id"=>"asc"];
+      $games = $em->getRepository('FantasyFootballTournamentCoreBundle:Game')
+                  ->findBy($criterias,$order);
+      return $games;
+  }
+
+  protected function _isTeamEdition($editionId)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $edition = $em->getRepository('FantasyFootballTournamentCoreBundle:Edition')
+                ->findOneById($editionId);
+    if (null === $edition){
+        return FALSE;
+    }else{
+        return (0 !== $edition->getFullTriplette());
+    }
+  }
+
+  protected function _summarizeTeam($games, $edition, $round)
+  {
+      $confrontations = [];
+      $currentFirstCoachTeam = 0;
+      foreach ($games as $game){
+          if ($currentFirstCoachTeam !== $game->getCoach1()->getCoachTeam()->getId()){
+            if( 0 !== $currentFirstCoachTeam ){
+                $confrontations[] = $confrontation;
+            }
+            $currentFirstCoachTeam = $game->getCoach1()->getCoachTeam()->getId();
+            $confrontation= new \stdClass;
+            $confrontation->team1 = $game->getCoach1()->getCoachTeam();
+            $confrontation->team2 = $game->getCoach2()->getCoachTeam();
+            $confrontation->games = [];
+          }
+          $confrontation->games[] = $game;
+      }
+      $confrontations[] = $confrontation;
+      return $this->render('FantasyFootballTournamentAdminBundle:Game:summarize_team.html.twig',
+                           array(
+                               'edition'=>$edition,
+                               'round'=>$round,
+                               'confrontations'=>$confrontations));
+  }
+
+  protected function _summarize($games, $edition, $round)
+  {
+      return $this->render('FantasyFootballTournamentAdminBundle:Game:summarize.html.twig',
+                           array(
+                               'edition'=>$edition,
+                               'round'=>$round,
+                               'games'=>$games));
+  }
+
+  public function summarizeAction(Request $request, $edition, $round)
+  {
+    $games = $this->_gamesByEditionAndRound($edition, $round);
+    if($this->_isTeamEdition($edition)){
+        return $this->_summarizeTeam($games, $edition, $round);
+    }else{
+        return $this->_summarize($games, $edition, $round);
+    }
   }
 }
